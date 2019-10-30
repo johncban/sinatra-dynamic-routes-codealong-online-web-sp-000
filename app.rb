0d09808f15250e76e74a554db437f54e3b7cd190
Juan Carlo Banayo <johncban@gmail.com>
2019-10-30 11:49:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ class App < Sinatra::Base
     "Hello #{@user_name}!"
   end
 
+
+  # Code your final two routes here:
   get "/goodbye/:name" do
     @name = params[:name]
     "Goodbye, #{@name}."
@@ -23,8 +25,5 @@ class App < Sinatra::Base
     "#{@product}"
   end
 
-  
-
-  # Code your final two routes here:
 
 end
